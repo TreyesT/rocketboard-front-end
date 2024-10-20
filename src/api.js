c1de37c185a294ed561f05a8c9a07e85f863a938
Tony-Reyes <jreyes29@stedwards.edu>
2024-10-20 17:04:11 -0500

    Changed api.js
    Test for env

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-console.log('API Base URL:', process.env.REACT_APP_API_URL);
+// console.log('Test variable:', process.env.REACT_APP_TEST_VARIABLE);
+console.log('Cool variable:', process.env.REACT_APP_API_URL);
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
