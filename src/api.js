174487245e3b23967bd59e56dbeaf1b7298ed036
Tony-Reyes <jreyes29@stedwards.edu>
2024-10-20 17:06:52 -0500

    Changed api.js
    test

--- src/api.js
+++ src/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// console.log('Test variable:', process.env.REACT_APP_TEST_VARIABLE);
 console.log('Cool variable:', process.env.REACT_APP_API_URL);
 
 const api = axios.create({
